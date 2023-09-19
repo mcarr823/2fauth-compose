f6fe237dfa281f1c29555520c6c1340476325728
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-09-19 21:59:34 +1000

    Implemented base classes for known exception types

---
+++ app/src/main/java/com/example/a2fauthcompose/data/exceptions/Auth2FException.kt
@@ -0,0 +1,44 @@
+package com.example.a2fauthcompose.data.exceptions
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Auth2FException(
+    override val message: String,
+) : Exception()
+
+//bad request
+@Serializable
+data class Auth2FException400(
+    override val message: String
+) : Exception()
+
+//Unauthenticated
+@Serializable
+data class Auth2FException401(
+    override val message: String
+) : Exception()
+
+//Forbidden
+@Serializable
+data class Auth2FException403(
+    override val message: String
+) : Exception()
+
+//Not Found
+@Serializable
+data class Auth2FException404(
+    override val message: String
+) : Exception()
+
+//Invalid data
+@Serializable
+data class Auth2FException422(
+    override val message: String
+) : Exception()
+
+//Internal Server Error
+@Serializable
+data class Auth2FException500(
+    override val message: String
+) : Exception()
