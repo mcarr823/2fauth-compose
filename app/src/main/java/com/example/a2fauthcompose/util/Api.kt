0b10a1d89dcd7ef0c63f7d851c88149ec07a6b51
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-09-19 22:02:55 +1000

    Implemented api requests for most twofaccounts REST API endpoints

--- app/src/main/java/com/example/a2fauthcompose/util/Api.kt
+++ app/src/main/java/com/example/a2fauthcompose/util/Api.kt
@@ -44,4 +44,50 @@ class Api(
 
     private suspend fun delete(path: String) =
         httpUtil.delete(path)
+
+
+
+    //twofaccounts
+
+    // Get all 2FA accounts
+    // Find all 2FA accounts of the authenticated user
+    suspend fun getAll2FaAccounts(withSecret: Boolean): List<Account> =
+        get("/api/v1/twofaccounts?withSecret=$withSecret").body()
+
+    // Create 2FA account
+    // Creates a new 2FA account for the authenticated user
+    suspend fun create2FaAccount(req: CreateAccountRequest): Account =
+        post("/api/v1/twofaccounts"){
+            setBody(req)
+        }.body()
+
+    // Mass delete 2FA accounts
+    // Mass deletes 2FA accounts of the authenticated user matching the IDs passed as query parameter.
+    suspend fun delete2FaAccount(ids: IntArray): Boolean =
+        delete("/api/v1/twofaccounts?ids="+ids.joinToString(","))
+
+    // Find 2FA account by ID
+    // Returns a single 2FA account of the authenticated user
+    suspend fun get2FaAccount(id: Int, withSecret: Boolean): Account =
+        get("/api/v1/twofaccounts/$id?withSecret=$withSecret").body()
+
+    // Update 2FA account
+    // Updates a 2FA account of the authenticated user
+    suspend fun update2FaAccount(id: Int, req: UpdateAccountRequest): Account =
+        put(" /api/v1/twofaccounts/$id"){
+            setBody(req)
+        }.body()
+
+    // Delete 2FA account
+    // Deletes a 2FA account of the authenticated user
+    suspend fun delete2FaAccount(id: Int): Boolean =
+        delete(" /api/v1/twofaccounts/$id")
+
+    // Not implemented:
+    //  /api/v1/twofaccounts/migration
+    //  /api/v1/twofaccounts/preview
+    //  /api/v1/twofaccounts/reorder
+    //  /api/v1/twofaccounts/withdraw
+    //  /api/v1/twofaccounts/export
+    //  /api/v1/groups/{id}/twofaccounts
 }
