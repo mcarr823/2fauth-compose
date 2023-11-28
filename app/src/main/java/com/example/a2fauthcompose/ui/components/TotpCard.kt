b09a8f71f320e5703809e0fc3ecf9bbfc340c431
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-11-28 20:15:21 +1100

    Split common card layout into separate class

--- app/src/main/java/com/example/a2fauthcompose/ui/components/TotpCard.kt
+++ app/src/main/java/com/example/a2fauthcompose/ui/components/TotpCard.kt
@@ -1,34 +1,21 @@
 package com.example.a2fauthcompose.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.a2fauthcompose.data.classes.AbstractAccount
-import com.example.a2fauthcompose.data.classes.Account
-import com.example.a2fauthcompose.data.classes.OTP
-import com.example.a2fauthcompose.data.classes.TotpAccount
 import com.example.a2fauthcompose.data.classes.TotpToken
-import com.example.a2fauthcompose.util.TokenUtil
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TotpCard(
     name: String,
@@ -47,22 +34,14 @@ fun TotpCard(
         //TODO: show toast
     }
 
-    ElevatedCard(
-        modifier = Modifier.padding(4.dp),
-        onClick = onTap
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-                .padding(vertical = 8.dp, horizontal = 16.dp)
+    PaddedCard(onTap = onTap) {
+        Text(text = name)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = name)
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = code)
-                CircularProgressIndicator()
-            }
+            Text(text = code)
+            CircularProgressIndicator()
         }
     }
 
