dc677f8fda816398da7455ad59272bf076f22e72
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-11-28 20:23:39 +1100

    Added classes for using Account data in layouts

---
+++ app/src/main/java/com/example/a2fauthcompose/data/classes/AbstractAccount.kt
@@ -0,0 +1,37 @@
+package com.example.a2fauthcompose.data.classes
+
+import dev.turingcomplete.kotlinonetimepassword.HmacAlgorithm
+
+/**
+ * @param id ID of the 2FA account
+ * @param groupId The ID of the group the 2FA account belongs to
+ * @param service The Issuer of the 2FA account
+ * @param account The Label of the 2FA account
+ * @param icon The filename of the icon which decorate the 2FA account
+ * @param secret A base32 encoded string used by the cryptographic algorithm
+ * to generate the One-Time Password.
+ * @param digits The number of digits of the generated One-Time Password
+ * @param algorithm The algorithm used to generate the One-Time Password.
+ * sha1, sha256, sha512, or md5
+ **/
+abstract class AbstractAccount(
+    val id: Int,
+    val groupId: Int?,
+    val service: String?,
+    val account: String,
+    val icon: String?,
+    val digits: Int,
+    val algorithm: String,
+    val secret: String?
+) {
+
+    abstract fun generate(secret: ByteArray): AbstractToken
+
+    fun getAlgorithm(): HmacAlgorithm = when(algorithm){
+        "sha1" -> HmacAlgorithm.SHA1
+        "sha256" -> HmacAlgorithm.SHA256
+        "sha512" -> HmacAlgorithm.SHA512
+        else -> throw Exception("Unknown algorithm")
+    }
+
+}
---
+++ app/src/main/java/com/example/a2fauthcompose/data/classes/TotpAccount.kt
@@ -0,0 +1,66 @@
+package com.example.a2fauthcompose.data.classes
+
+import dev.turingcomplete.kotlinonetimepassword.TimeBasedOneTimePasswordConfig
+import dev.turingcomplete.kotlinonetimepassword.TimeBasedOneTimePasswordGenerator
+import java.util.concurrent.TimeUnit
+
+/**
+ * @param id ID of the 2FA account
+ * @param group_id The ID of the group the 2FA account belongs to
+ * @param service The Issuer of the 2FA account
+ * @param account The Label of the 2FA account
+ * @param icon The filename of the icon which decorate the 2FA account
+ * @param otp_type The type of 2FA account. Either "totp" or "hotp"
+ * @param secret A base32 encoded string used by the cryptographic algorithm
+ * to generate the One-Time Password.
+ * @param digits The number of digits of the generated One-Time Password
+ * @param algorithm The algorithm used to generate the One-Time Password.
+ * sha1, sha256, sha512, or md5
+ * @param period For TOTP only. The validity duration of One-Time Password
+ * generated for the account
+ **/
+class TotpAccount(
+    id: Int,
+    groupId: Int?,
+    service: String?,
+    account: String,
+    icon: String?,
+    digits: Int,
+    algorithm: String,
+    secret: String?,
+    val period: Int
+) : AbstractAccount(
+    id, groupId, service, account, icon, digits, algorithm, secret
+) {
+
+    constructor(a: Account, period: Int) : this(
+        id = a.id,
+        groupId = a.group_id,
+        service = a.service,
+        account = a.account,
+        icon = a.icon,
+        digits = a.digits,
+        algorithm = a.algorithm,
+        secret = a.secret,
+        period = period
+    )
+
+    override fun generate(secret: ByteArray): TotpToken {
+        val config = TimeBasedOneTimePasswordConfig(
+            codeDigits = digits,
+            hmacAlgorithm = getAlgorithm(),
+            timeStep = period.toLong(),
+            timeStepUnit = TimeUnit.SECONDS
+        )
+        val timeBasedOneTimePasswordGenerator = TimeBasedOneTimePasswordGenerator(
+            secret,
+            config
+        )
+        val password = timeBasedOneTimePasswordGenerator.generate()
+        val counter = timeBasedOneTimePasswordGenerator.counter()
+        //The start of next time slot minus 1ms
+        val endEpochMillis = timeBasedOneTimePasswordGenerator.timeslotStart(counter+1)-1
+        return TotpToken(password = password, expiresAt = endEpochMillis)
+    }
+
+}
