8ebf95c5394e3e4c8bd43b7ae941125fb52feb3a
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-12-16 19:03:01 +1100

    Added concept of left and right action bar buttons, and fab button, to screen previews

--- app/src/main/java/com/example/a2fauthcompose/ui/screens/PreviewScreen.kt
+++ app/src/main/java/com/example/a2fauthcompose/ui/screens/PreviewScreen.kt
@@ -2,17 +2,24 @@ package com.example.a2fauthcompose.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a2fauthcompose.ui.theme._2FAuthComposeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PreviewScreen(
+    backButton: @Composable () -> Unit,
+    doneButton: @Composable () -> Unit,
+    fab: @Composable () -> Unit,
     content: @Composable () -> Unit
 ) {
 
@@ -23,6 +30,15 @@ fun PreviewScreen(
         ) {
             Scaffold(
                 modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = "2FAuth") },
+                        navigationIcon = { backButton() },
+                        actions = { doneButton() }
+                    )
+                },
+                floatingActionButton = { fab() },
+                floatingActionButtonPosition = FabPosition.End
             ) {
                 Surface(
                     modifier = Modifier.padding(it)
