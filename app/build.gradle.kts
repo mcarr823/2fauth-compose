f639d2304b0510380eb278d69d07704aeee7e05b
mcarr <136939846+mcarr823@users.noreply.github.com>
2024-06-15 14:54:32 +1000

    Initial user interface and navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,6 +87,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material")
     implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
